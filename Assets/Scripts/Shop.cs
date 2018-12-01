9469755e226b6c83d7b5bc03a2dc348d154b04c3
Bryson <esf704@my.utsa.edu>
2018-12-01 16:43:20 -0600

    Another commit

---
+++ Assets/Scripts/Shop.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour {
+
+    BuildManager buildManager;
+
+    void Start()
+    {
+        buildManager = BuildManager.instance;
+    }
+	public void PurchaseStandardTurret()
+    {
+        print("Standard Turret Purchased");
+        buildManager.SetTurretToBuild(buildManager.standardTurretPrefab);
+    }
+
+    public void PurchaseMissileLauncherTurret()
+    {
+        print("Missle Launcher Purchased");
+        buildManager.SetTurretToBuild(buildManager.missleLauncherPrefab);
+    }
+}
